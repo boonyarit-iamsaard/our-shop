b5602e60283a49698987655f5d496fc4c505e3e8
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-05-31 21:16:58 +0700

    build: update site header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 
+import { HomeIcon } from 'lucide-react';
+
 import { Button } from '~/components/ui/button';
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 bg-neutral-300 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
-        <Button asChild variant="ghost">
-          <Link href="/">Vacation Club</Link>
+      <div className="container flex h-14 items-center justify-between">
+        <Button asChild variant="ghost" size="icon">
+          <Link href="/">
+            <HomeIcon className="h-6 w-6" />
+          </Link>
         </Button>
         <nav className="flex items-center gap-4">
           <Button asChild variant="ghost">
